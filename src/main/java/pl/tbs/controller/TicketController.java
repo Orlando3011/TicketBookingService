e3640d34b85824b2da74b5b9593a7c47befd8a1b
Orlando3011 <34520084+Orlando3011@users.noreply.github.com>
2019-12-25 13:16:31 +0100

    Fixed ticket reservation

--- src/main/java/pl/tbs/controller/TicketController.java
+++ src/main/java/pl/tbs/controller/TicketController.java
@@ -3,8 +3,10 @@ package pl.tbs.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import pl.tbs.model.Event;
 import pl.tbs.model.Reservation;
 import pl.tbs.model.Ticket;
+import pl.tbs.repository.EventRepository;
 import pl.tbs.repository.ReservationRepository;
 import pl.tbs.repository.TicketRepository;
 
@@ -16,26 +18,37 @@ public class TicketController {
     private TicketRepository tickets;
     @Autowired
     private ReservationRepository reservations;
+    @Autowired
+    private EventRepository events;
 
     @GetMapping("/users/{userId}/reservations/{reservationId}/tickets")
     public List<Ticket> showTicketsInReservation(@RequestParam("reservationId") int id) {
         return reservations.findById(id).getTickets();
     }
 
-    @PostMapping("/users/{userId}/reservations/{reservationId}/tickets")
-    public void addTicket(@RequestParam("reservationId") int id, @RequestBody Ticket ticket) {
-        Reservation reservation = reservations.findById(id);
+    @PostMapping("/users/{userId}/reservations/{reservationId}/events/{eventId}/tickets")
+    public void addTicket(@RequestParam("reservationId") int reservationId, @RequestParam("eventId") int eventId, @RequestBody Ticket ticket) {
+
+        Event event = events.findById(eventId);
+        ticket.setEvent(event);
+        event.setTicketsAvailable(event.getTicketsAvailable() - 1);
+
+        Reservation reservation = reservations.findById(reservationId);
         reservation.addTicket(ticket);
         tickets.save(ticket);
         reservations.save(reservation);
+        events.save(event);
     }
 
-    @DeleteMapping("/users/{userId}/reservations/{reservationId}/tickets/{ticketId}")
-    public void removeTicket(@RequestParam("reservationId") int reservationId, @RequestParam("ticketId") int ticketId) {
+    @DeleteMapping("/users/{userId}/reservations/{reservationId}/events/{eventId}/tickets/{ticketId}")
+    public void removeTicket(@RequestParam("reservationId") int reservationId, @RequestParam("ticketId") int ticketId, @RequestParam("eventId") int eventId) {
+        Event event = events.findById(eventId);
+        event.setTicketsAvailable(event.getTicketsAvailable() + 1);
         Reservation reservation = reservations.findById(reservationId);
         Ticket ticket = tickets.findById(ticketId);
         reservation.removeTicket(ticket);
         tickets.delete(ticket);
         reservations.save(reservation);
+        events.save(event);
     }
 }
